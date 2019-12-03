4449434a5f21084c5a22153e10ab30cbfe9809ab
Kevin Gigiano <kgigiano@harris.com>
2019-12-03 16:41:28 -0700

    Update main

--- main.tf
+++ main.tf
@@ -2,11 +2,59 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+data "aws_vpc" "default" {
+  default = true
+}
 
-  tags = {
-    Name = "terraform-example"
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+
+  min_size = 2
+  max_size = 10
+
+
+  tag  {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_configuration" "example" {
+  image_id      = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hi Mom!" > index.html
+              nohup busybox httpd -f -p ${var.server_port}  &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
